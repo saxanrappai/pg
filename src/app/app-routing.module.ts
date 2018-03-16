e87c30d2daabe012f8d6a9941f6afa7d861bcb5f
sesa378818 <josephsaxan.rappai@schneider-electric.com>
2018-03-16 23:47:29 +0100

    added navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PeoplesComponent }      from './peoples/peoples.component';
+import { PeopleDetailsComponent } from './people-details/people-details.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { AddPeopleComponent } from './add-people/add-people.component';
 
 const routes: Routes = [
   { path: '', component: PeoplesComponent },
   { path: 'peoples', component: PeoplesComponent },
+  { path: 'people-details', component: PeopleDetailsComponent },
+  { path: 'payments', component: PaymentsComponent },
+  { path: 'payment-details', component: PaymentDetailsComponent },
+  { path: 'add-people', component: AddPeopleComponent },
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import 'bootstrap';
+import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { PeoplesComponent } from './peoples/peoples.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { PeopleDetailsComponent } from './people-details/people-details.component';
 import { PaymentsComponent } from './payments/payments.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { AddPeopleComponent } from './add-people/add-people.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
@@ -16,11 +18,14 @@ import { PaymentDetailsComponent } from './payment-details/payment-details.compo
     PeoplesComponent,
     PeopleDetailsComponent,
     PaymentsComponent,
-    PaymentDetailsComponent
+    PaymentDetailsComponent,
+    AddPeopleComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
 		AppRoutingModule,
+		AlertModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
