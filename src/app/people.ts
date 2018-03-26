bca380fb8d97902ce07191957f11dc339a9f3d0e
sesa378818 <josephsaxan.rappai@schneider-electric.com>
2018-03-26 15:12:58 +0200

    added eslint config file

---
+++ src/app/people.ts
@@ -0,0 +1,11 @@
+export class People {
+
+  constructor(
+    public name: string,
+    public mobile: string,
+    public address: string,
+    public pin: string,
+    public date: string
+  ) {  }
+
+}
